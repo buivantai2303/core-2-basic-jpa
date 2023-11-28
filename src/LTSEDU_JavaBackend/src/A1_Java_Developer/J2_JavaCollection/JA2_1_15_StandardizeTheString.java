e2378c98110796662e9b017d7db35f635ee7c279
buivantai2303 <buivantai2303@gmail.com>
2023-11-28 16:14:15 +0700

    Changed by LuHanhThu53

--- src/LTSEDU_JavaBackend/src/A1_Java_Developer/J2_JavaCollection/JA2_1_15_StandardizeTheString.java
+++ src/LTSEDU_JavaBackend/src/A1_Java_Developer/J2_JavaCollection/JA2_1_15_StandardizeTheString.java
@@ -1,26 +1,31 @@
 package A1_Java_Developer.J2_JavaCollection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class JA2_1_15_StandardizeTheString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> name = new ArrayList<>();
 
         System.out.print("Enter your full name: ");
-        String Line_1 = scanner.nextLine();
+        String fullName = scanner.nextLine();
 
-
-
-//        String email = Scan(fullName);
-//        System.out.println("Your email: " + email);
-
+        String email = generateEmail(fullName);
+        System.out.println("Your email: " + email);
     }
 
-    public static String Scan(String fullName){
-        String Email = fullName.replaceAll("\\s+", "").toLowerCase();
-        return Email + "@hvitclan.com";
+    public static String generateEmail(String fullName) {
+        List<String> names = Arrays.asList(fullName.split("\\s+"));
+
+        StringBuilder firstEmail = new StringBuilder();
+        for (int i = 0; i < names.size() - 1; i++) {
+            firstEmail.append(names.get(i).substring(0, 1).toUpperCase());
+        }
+
+        String lastEmail = names.get(names.size() - 1).substring(0, 1).toUpperCase() + names.get(names.size() - 1).substring(1).toLowerCase();
+
+        return lastEmail + firstEmail + "@hvitclan.com";
     }
 }
