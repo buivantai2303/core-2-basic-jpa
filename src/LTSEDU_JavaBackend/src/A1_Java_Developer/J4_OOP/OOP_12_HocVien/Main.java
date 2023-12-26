5b80445c236387768d53427eb3e45b00c294f417
buivantai2303 <buivantai2303@gmail.com>
2023-12-26 17:00:45 +0700

    Changed by LuHanhThu53

---
+++ src/LTSEDU_JavaBackend/src/A1_Java_Developer/J4_OOP/OOP_12_HocVien/Main.java
@@ -0,0 +1,26 @@
+package A1_Java_Developer.J4_OOP.OOP_12_HocVien;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        HocVien hocVien = new HocVien();
+
+        hocVien.InThongTin();
+
+        List<HocVien> list = new ArrayList<HocVien>();
+
+        list.add(hocVien);
+        list.add(new HocVien());
+        list.add(new HocVien());
+        list.add(new HocVien());
+        list.add(new HocVien());
+
+
+        List<HocVien> listResult = HocVien.TimKiemHocVien(list);
+        for (HocVien i : listResult) {
+            i.InThongTin();
+        }
+    }
+}
