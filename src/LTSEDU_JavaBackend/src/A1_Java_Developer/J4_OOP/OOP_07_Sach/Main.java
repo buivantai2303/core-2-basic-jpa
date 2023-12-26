c8fdcd425785b6bccd4e658a8c91e48552895756
buivantai2303 <buivantai2303@gmail.com>
2023-12-26 14:37:01 +0700

    Changed by LuHanhThu53

---
+++ src/LTSEDU_JavaBackend/src/A1_Java_Developer/J4_OOP/OOP_07_Sach/Main.java
@@ -0,0 +1,9 @@
+package A1_Java_Developer.J4_OOP.OOP_07_Sach;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassSach classSach = new ClassSach();
+
+        classSach.InThongTin();
+    }
+}
